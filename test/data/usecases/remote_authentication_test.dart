5dafb11a9aaab9f885b2934653fde34fddced179
KailanySousa <kailany-sousa2010@hotmail.com>
2023-08-21 19:40:34 -0300

    ensure RemoteAuthentication calls HttpClient with correct Body

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -1,7 +1,33 @@
+import 'package:clean_flutter_app/domain/usecases/authentication.dart';
 import 'package:faker/faker.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+
+abstract class HttpClient {
+  Future<void> request({
+    @required String url,
+    @required String method,
+    Map body,
+  }) async {}
+}
+
+class HttpClientSpy extends Mock implements HttpClient {}
+
+class RemoteAuthentication {
+  final HttpClient httpClient;
+  final String url;
+
+  RemoteAuthentication({
+    @required this.httpClient,
+    @required this.url,
+  });
+  Future<void> auth(AuthenticationParams params) async {
+    final body = {'email': params.email, 'password': params.secret};
+
+    await httpClient.request(url: url, method: 'post', body: body);
+  }
+}
 
 void main() {
   RemoteAuthentication sut;
@@ -18,36 +44,17 @@ void main() {
     );
   });
   test('Should call HttpClient with correct values', () async {
-    await sut.auth();
+    final params = AuthenticationParams(
+      email: faker.internet.email(),
+      secret: faker.internet.password(),
+    );
+
+    await sut.auth(params);
 
     verify(httpClient.request(
       url: url,
       method: 'post',
+      body: {'email': params.email, 'password': params.secret},
     ));
   });
-}
-
-abstract class HttpClient {
-  Future<void> request({
-    @required String url,
-    @required String method,
-  }) async {}
-}
-
-class HttpClientSpy extends Mock implements HttpClient {}
-
-class RemoteAuthentication {
-  final HttpClient httpClient;
-  final String url;
-
-  RemoteAuthentication({
-    @required this.httpClient,
-    @required this.url,
-  });
-  Future<void> auth() async {
-    await httpClient.request(
-      url: url,
-      method: 'post',
-    );
-  }
 }
