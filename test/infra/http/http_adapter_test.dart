560e801b8953496ee45870f82801b8286b1441b6
KailanySousa <kailany-sousa2010@hotmail.com>
2023-09-12 19:27:17 -0300

    ensure HttpAdapter throws BadRequestError on 400

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,3 +1,4 @@
+import 'package:clean_flutter_app/data/http/http.dart';
 import 'package:clean_flutter_app/infra/http/http.dart';
 import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -93,6 +94,17 @@ void main() {
       );
 
       expect(response, null);
+    });
+
+    test('Should return BadRequestError if post returns 400', () async {
+      mockResponse(400);
+
+      final future = sut.request(
+        url: url,
+        method: 'post',
+      );
+
+      expect(future, throwsA(HttpError.badRequest));
     });
   });
 }
