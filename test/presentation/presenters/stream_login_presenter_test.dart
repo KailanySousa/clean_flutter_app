fbd0e132d71dbada55b84e4999b73b96869e9418
KailanySousa <kailany-sousa2010@hotmail.com>
2023-09-25 19:44:28 -0300

    move Validation to its own file

--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -1,13 +1,10 @@
 import 'dart:async';
 
+import 'package:meta/meta.dart';
+import 'package:clean_flutter_app/presentation/protocols/validation.dart';
 import 'package:faker/faker.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
-
-abstract class Validation {
-  validate({@required String field, @required String value});
-}
 
 class LoginState {
   String emailError;
