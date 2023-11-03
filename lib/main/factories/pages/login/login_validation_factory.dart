5eab2a75a1f99dc15e890aa12402a4a0164e75a5
KailanySousa <kailany-sousa2010@hotmail.com>
2023-11-03 11:48:51 -0300

    add ValidationBuilder

--- lib/main/factories/pages/login/login_validation_factory.dart
+++ lib/main/factories/pages/login/login_validation_factory.dart
@@ -1,3 +1,4 @@
+import 'package:clean_flutter_app/main/builders/builders.dart';
 import 'package:clean_flutter_app/presentation/protocols/protocols.dart';
 import 'package:clean_flutter_app/validation/protocols/protocols.dart';
 import 'package:clean_flutter_app/validation/validators/validators.dart';
@@ -6,8 +7,7 @@ Validation makeLoginValidation() => ValidationComposite(makeLoginValidations());
 
 List<FieldValidation> makeLoginValidations() {
   return [
-    RequiredFieldValidation('email'),
-    EmailValidation('email'),
-    RequiredFieldValidation('password'),
+    ...ValidationBuilder.field('email').required().email().build(),
+    ...ValidationBuilder.field('password').required().build(),
   ];
 }
